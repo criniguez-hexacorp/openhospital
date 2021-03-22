7526bb3fe1894f7f74a7bfeb3681ce250a3fc426
criniguez-hexacorp <cristian.iniguez@hexacorp.com.bo>
2021-03-22 16:56:20 +0000

    adding sequence

--- __manifest__.py
+++ __manifest__.py
@@ -21,6 +21,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/ir_sequence_data.xml',
         'views/patient_views.xml',
         'views/menus.xml',
     ],
--- models/patient.py
+++ models/patient.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class HospitalPatient(models.Model):
@@ -8,6 +8,18 @@ class HospitalPatient(models.Model):
     _rec_name = 'name'
 
     name = fields.Char(string='Name', required=True)
+    name_seq = fields.Char(
+        string='Order Reference', required=True, copy=False, readonly=True, index=True, default=lambda self: _('New')
+    )
     age = fields.Integer(string='Age')
     notes = fields.Text(string='Notes')
     image = fields.Binary(string='Image')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name_seq', _('New')) == _('New'):
+            vals['name_seq'] = self.env['ir.sequence'].next_by_code(
+                'openhospital.patient') or _('New')
+
+        result = super(HospitalPatient, self).create(vals)
+        return result
