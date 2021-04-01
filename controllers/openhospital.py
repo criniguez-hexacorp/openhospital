40ec7ebf55d6516491bd28d515723570a894b157
criniguez-hexacorp <cristian.iniguez@hexacorp.com.bo>
2021-04-01 17:53:51 +0000

    web controllers auth public and auth user difference

--- controllers/openhospital.py
+++ controllers/openhospital.py
@@ -3,7 +3,7 @@ from odoo import http
 
 
 class Hospital(http.Controller):
-    @http.route('/openhospital/patient/', website=True, auth='public')
+    @http.route('/openhospital/patient/', website=True, auth='user')
     def hospital_doctor(self, **kw):
         patients = http.request.env['openhospital.patient'].sudo().search([])
         return http.request.render('openhospital.patients', {
